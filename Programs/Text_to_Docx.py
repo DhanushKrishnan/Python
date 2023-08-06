0168b3ef5b3fc2105acf9b3c47697fed44f3ee79
Dhanush <dhanushkrish54@gmail.com>
2023-08-06 10:09:03 +0530

    Update Text_to_Docx.py

--- Programs/Text_to_Docx.py
+++ Programs/Text_to_Docx.py
@@ -11,7 +11,7 @@ def text_to_docx(input_file, output_file):
 
 # Replace 'input.txt' with the path of your input text file,
 # and 'output.docx' with the desired name of the converted Word document.
-input_file = 'C:\Dhanush\Documents\Datavalley\Python\Imports\Python Interview Questions & Answers(40).txt'
-output_file = 'C:\Dhanush\Documents\Datavalley\Python\Exports\(40)Python Interview Questions & Answers.docx'
+input_file = 'path'
+output_file = 'path'
 
 text_to_docx(input_file, output_file)
